a4ed34b3eda818b971359054ab68a02b0445b94f
Andre <sidojoyo567@gmail.com>
2024-12-17 13:15:22 +0700

    commit pertama

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,13 @@
 use App\Http\Controllers\Todo\TodoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 route::get('/halo',function(){
     return view('coba.halo');
 });
 
 
-route::get('/todo',[TodoController::class,'index'])->name('todo');
-route::post('/todo',[TodoController::class,'store'])->name('todo.post');
-route::put('/todo/{id}',[TodoController::class,'update'])->name('todo.update');
-route::delete('/todo/{id}',[TodoController::class,'destroy'])->name('todo.delete');
+route::get('/',[TodoController::class,'index'])->name('todo');
+route::post('/',[TodoController::class,'store'])->name('todo.post');
+route::put('/{id}',[TodoController::class,'update'])->name('todo.update');
+route::delete('/{id}',[TodoController::class,'destroy'])->name('todo.delete');
